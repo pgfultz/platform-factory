8ace71b2bce863753a4b7ce13e7aaeb82d3551a5
Lucas Morais <lucas.s.morais23@gmail.com>
2020-09-29 13:31:20 -0300

    Some changes

--- config.php
+++ config.php
@@ -3,10 +3,8 @@
 date_default_timezone_set("America/Sao_Paulo");
 
 define('BASE', 'http://localhost/platform-factory');
-//define('BASE', 'http://platformfactory.co.uk');
 
 $pdo = new PDO('mysql:host=localhost;dbname=platform-factory', 'root', '');
-//$pdo = new PDO('mysql:host=localhost;dbname=u207526276_platformfactor', 'u207526276_platformfactor', 'Platformfactory@*2020');
 $pdo->exec("set names utf8");
 
 $url = (isset($_GET['url']) ? $_GET['url'] : 'home');
